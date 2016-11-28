cfce4ba6726eca3da79e13dc1bb74b817bbd3c9a
Max <maxsizen@gmail.com>
2016-11-28 17:32:08 +0400

    1st version

---
+++ Bindings.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class Bindings : MonoBehaviour {
+
+	private static Bindings _instance;
+
+	List<Binding> autoBindings;
+	List<Binding> invalidBindings;
+	List<Binding> allBindings;
+
+	public static Bindings Instance
+	{
+		get
+		{
+			if (_instance == null)
+			{
+				_instance = FindObjectOfType(typeof(Bindings)) as Bindings;
+				_instance.init();
+			}
+			return _instance;
+		}
+	}
+
+    private void init()
+    {
+		autoBindings = new List<Binding>();
+		invalidBindings = new List<Binding>();
+		allBindings = new List<Binding>();
+    }
+
+    public void addBinding(Binding binding, bool auto = false)
+	{
+		if (allBindings.Contains(binding))
+			return;
+		List<Binding> list = auto ? autoBindings : invalidBindings;
+		list.Add(binding);
+		allBindings.Add(binding);
+	}
+
+	public void removeBinding(Binding binding)
+	{
+		removeFromList(allBindings, binding);
+		removeFromList(autoBindings, binding);
+		removeFromList(invalidBindings, binding);
+	}
+
+	public void invalid(object source, string propertyName)
+	{
+		List<Binding> list = findBindings(source, propertyName);
+		foreach (var item in list)
+		{
+			if (!invalidBindings.Contains(item))
+				invalidBindings.Add(item);
+		}
+	}
+
+	List<Binding> findBindings(object value, string propertyName)
+	{
+		List<Binding> list = new List<Binding>();
+		foreach (var item in allBindings)
+		{
+			if (item.fromValue == value && item.fromPropertyName == propertyName)
+				list.Add(item);
+		}
+		return list;
+	}
+
+	void removeFromList(List<Binding> list, Binding binding)
+	{
+		if (list.Contains(binding))
+		{
+			list.RemoveAt(list.IndexOf(binding));
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		apply();
+	}
+
+    private void apply()
+    {
+		applyList(autoBindings);
+		applyList(invalidBindings);
+    }
+
+    private void applyList(List<Binding> list)
+    {
+		foreach (var item in list)
+		{
+			item.apply();
+		}
+    }
+}
